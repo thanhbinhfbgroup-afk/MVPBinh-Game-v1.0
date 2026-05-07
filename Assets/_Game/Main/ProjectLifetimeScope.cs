0ae99ea0cb928974a7e9a6f769f90da400abb439
thanhbinhfbgroup-afk <thanhbinh.fbgroup@gmail.com>
2026-05-07 17:27:49 +0700

    i

--- Assets/_Game/Main/ProjectLifetimeScope.cs
+++ Assets/_Game/Main/ProjectLifetimeScope.cs
@@ -3,7 +3,7 @@ using BillGameCore.Interfaces.Signals;
 using VContainer;
 using VContainer.Unity;
 using MessagePipe;
-
+using BillGameCore.Modules.Input;
 public class ProjectLifetimeScope : LifetimeScope
 {
     protected override void Configure(IContainerBuilder builder)
@@ -12,5 +12,7 @@ public class ProjectLifetimeScope : LifetimeScope
         var options = builder.RegisterMessagePipe();
         builder.RegisterMessageBroker<MoveInputSignal>(options);
         builder.RegisterMessageBroker<ActionInputSignal>(options);
+        // Đăng ký Service
+        builder.Register<IInputService, InputManager>(Lifetime.Singleton).AsImplementedInterfaces();
     }
 }
